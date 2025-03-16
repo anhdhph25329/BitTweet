c52df38e8b72b4929bd3cf1e2c56970f1a17362a
katoridao <hoanganhdao2k3@gmail.com>
2025-03-16 10:47:07 +0700

    1st commit

---
+++ app/src/main/java/fpt/anhdhph/bittweet/screen/ScreenWelcome.java
@@ -0,0 +1,38 @@
+package fpt.anhdhph.bittweet.screen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import fpt.anhdhph.bittweet.R;
+
+public class ScreenWelcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_screen_welcome);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(ScreenWelcome.this, ScreenLogin.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
+    }
+}
