cbdbf04e44cd522478240ba1f6ac25b550c4fdf8
katoridao <hoanganhdao2k3@gmail.com>
2025-03-21 13:25:40 +0700

    4th commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation(platform("com.google.firebase:firebase-bom:33.11.0"))
+    implementation("com.google.firebase:firebase-analytics")
+
 }
