eb46ffa5cdd94fc676422573a045edf985c60017
katoridao <hoanganhdao2k3@gmail.com>
2025-03-26 12:32:13 +0700

    6th commit

---
+++ app/src/main/java/fpt/anhdhph/bittweet/screen/ScreenRecover1.java
@@ -0,0 +1,38 @@
+package fpt.anhdhph.bittweet.screen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import fpt.anhdhph.bittweet.R;
+
+public class ScreenRecover1 extends AppCompatActivity {
+    Button btnXacNhan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_screen_recover1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btnXacNhan = findViewById(R.id.btnXacNhan);
+        btnXacNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ScreenRecover1.this, ScreenRecover2.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/fpt/anhdhph/bittweet/screen/ScreenRecover2.java
@@ -0,0 +1,39 @@
+package fpt.anhdhph.bittweet.screen;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import fpt.anhdhph.bittweet.R;
+
+public class ScreenRecover2 extends AppCompatActivity {
+
+    Button btnConfirm;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_screen_recover2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnConfirm = findViewById(R.id.btnConfirm);
+
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+}
